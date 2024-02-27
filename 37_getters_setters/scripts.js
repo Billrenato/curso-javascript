d716631504c0f3f531b41b01c9f3a1c0620c0905
Billrenato <billrenato0@gmail.com>
2024-02-27 18:54:50 -0300

    update

---
+++ 37_getters_setters/scripts.js
@@ -0,0 +1,13 @@
+class Cachorro{
+  constructor(raca, patas, cor){
+  this.raca = raca;
+  this.patas = patas;
+  this.cor = cor;
+  }
+}
+
+let labrador = new Cachorro('labradro', 4, 'preto');
+console.log(labrador.cor);
+
+
+
